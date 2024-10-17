8a153efeb3cbea3059f1ff2f68f5883b3562a8df
rutu1936 <rutuagade15@gmail.com>
2024-10-17 12:38:53 +0530

    add2

--- calculator/src/main/java/com/app/controller/HomeController.java
+++ calculator/src/main/java/com/app/controller/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-	
+	    
 	public void m1()
 	{
 		System.out.println("this is m1 method");
